3b7b6a30c6a97ef83c81129ddd4d3059ff87dad1
dathdka <dathdka@gmail.com>
2023-07-05 13:54:54 +0700

    add nationaly attribute  for driver

--- Services/api/src/migrations/20230702045946_add_drivers.ts
+++ Services/api/src/migrations/20230702045946_add_drivers.ts
@@ -4,9 +4,10 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("drivers", (table) => {
     table.increments("id").primary();
     table.string("name");
+    table.string("nationality");
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('drivers');
+  return knex.schema.dropTable("drivers");
 }
